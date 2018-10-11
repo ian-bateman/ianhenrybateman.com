a1ae75da6dd5671ddd59f237b42b1eaab50fafd7
Ian H. Bateman <ianhenrybateman@gmail.com>
2018-10-10 23:47:57 -0400

    more progress on about page

---
+++ profile.js
@@ -0,0 +1,20 @@
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("profile").src = images[x];
+}
+
+function displayPreviousImage() {
+    x = (x <= 0) ? images.length - 1 : x - 1;
+    document.getElementById("profile").src = images[x];
+}
+
+function startTimer() {
+    setInterval(displayNextImage, 3000);
+}
+
+var images = [], x = -1;
+images[0] = "images/Ian1.png";
+images[1] = "images/Ian2.jpg";
+images[2] = "images/Ian3.png";
+images[3] = "images/Ian4.jpg";
+images[4] = "images/Ian5.png";
