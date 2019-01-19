a79df62fe657a5407823a01d83fb69a710063154
Ian H. Bateman <ianhenrybateman@gmail.com>
2019-01-18 22:24:19 -0500

    updated about pictures

--- profile.js
+++ profile.js
@@ -13,9 +13,10 @@ function startTimer() {
 }
 
 var images = [], x = -1;
-images[0] = "images/Ian0.png";
+images[0] = "images/Ian0.jpg";
 images[1] = "images/Ian1.png";
 images[2] = "images/Ian2.png";
 images[3] = "images/Ian3.png";
-images[4] = "images/Ian4.png";
+images[4] = "images/Ian4.jpg";
 images[5] = "images/Ian5.png";
+images[6] = "images/Ian6.png";
