44131253e2bfd2f8a5892e09d66d3592acd19c8a
Ian H. Bateman <ianhenrybateman@gmail.com>
2019-10-06 09:41:10 -0400

    updated bio, profile pic, about page, email address

--- helpers/profile.js
+++ helpers/profile.js
@@ -1,17 +1,19 @@
 function displayNextImage() {
-    x = (x === images.length - 1) ? 0 : x + 1;
-    document.getElementById("profile").src = images[x];
+  x = x === images.length - 1 ? 0 : x + 1;
+  document.getElementById("profile").src = images[x];
 }
 
 function startTimer() {
-    setInterval(displayNextImage, 3000);
+  setInterval(displayNextImage, 3000);
 }
 
-var images = [], x = -1;
+var images = [],
+  x = -1;
 images[0] = "images/Ian0.jpg";
-images[1] = "images/Ian1.png";
+images[1] = "images/Ian1.jpg";
 images[2] = "images/Ian2.png";
-images[3] = "images/Ian3.jpg";
+images[3] = "images/Ian3.png";
 images[4] = "images/Ian4.jpg";
-images[5] = "images/Ian5.png";
+images[5] = "images/Ian5.jpg";
 images[6] = "images/Ian6.png";
+images[7] = "images/Ian7.png";
